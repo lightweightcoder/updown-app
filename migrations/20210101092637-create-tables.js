854cb658ad512a4ddd65d6166e4c6ec7f06e9a33
aljt <alvinljt@gmail.com>
2021-01-01 18:07:15 +0800

    implemented migration and seed data

---
+++ migrations/20210101092637-create-tables.js
@@ -0,0 +1,118 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      email: {
+        allowNull: false,
+        unique: true,
+        type: Sequelize.STRING,
+      },
+      password: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      hashedPassword: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      name: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+
+    await queryInterface.createTable('games', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      drawPile: {
+        type: Sequelize.JSON,
+      },
+      discardPileCard: {
+        type: Sequelize.JSON,
+      },
+      winnerId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'users',
+          key: 'id',
+        },
+      },
+      currentPlayerId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'users',
+          key: 'id',
+        },
+      },
+      status: {
+        // ongoing or cancelled
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+
+    await queryInterface.createTable('gamesUsers', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      gameId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'games',
+          key: 'id',
+        },
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'users',
+          key: 'id',
+        },
+      },
+      hand: {
+        type: Sequelize.JSON,
+      },
+      playerNum: {
+        type: Sequelize.INTEGER,
+      },
+      score: {
+        type: Sequelize.INTEGER,
+        defaultValue: 0,
+      },
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('users');
+    await queryInterface.dropTable('games');
+    await queryInterface.dropTable('gamesUsers');
+  },
+};
---
+++ seeders/20210101095902-fake-data.js
@@ -0,0 +1,54 @@
+// require jssha lib
+const jsSha = require('jssha');
+
+// function to generate a hash for password
+function getHash(input) {
+  // environment variable to use as a secret word for hashing userId cookie
+  // environment variable is currently stored in ~/.profile (see RA module 3.6.4)
+  const myEnvVar = process.env.MY_ENV_VAR;
+
+  // create new SHA object
+  // eslint-disable-next-line new-cap
+  const shaObj = new jsSha('SHA-512', 'TEXT', { encoding: 'UTF8' });
+
+  // create an unhashed cookie string based on user ID and myEnVar
+  const unhashedString = `${input}-${myEnvVar}`;
+
+  // generate a hashed cookie string using SHA object
+  shaObj.update(unhashedString);
+
+  return shaObj.getHash('HEX');
+}
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    const usersList = [
+      {
+        email: 'kai@kai.com',
+        password: 'kai',
+        hashedPassword: getHash('kai'),
+        name: 'kai',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        email: 'alvin@alvin.com',
+        password: 'alvin',
+        hashedPassword: getHash('alvin'),
+        name: 'alvin',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ];
+
+    await queryInterface.bulkInsert(
+      'users',
+      usersList,
+      { returning: true },
+    );
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    queryInterface.bulkDelete('users', null, {});
+  },
+};
