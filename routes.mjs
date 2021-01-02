cf407e1b49ef12627918d5c81aca576159665b90
aljt <alvinljt@gmail.com>
2021-01-02 11:54:28 +0800

    added authentication middleware

--- routes.mjs
+++ routes.mjs
@@ -1,9 +1,22 @@
 import { resolve } from 'path';
 import db from './models/index.mjs';
 
+// import controllers
+import games from './controllers/games.mjs';
+import users from './controllers/users.mjs';
+
+// import checkAuth middleware
+import checkAuthMiddleware from './lib/check-auth.mjs';
+
 export default function routes(app) {
-  // special JS page. Include the webpack index.html file
-  app.get('/home', (request, response) => {
+  // pass in db for all callbacks in controllers
+  const checkAuth = checkAuthMiddleware(db);
+
+  // login page
+  app.get('/login');
+
+  // main page, also the gameplay page
+  app.get('/', checkAuth, (request, response) => {
     response.sendFile(resolve('js/dist', 'index.html'));
   });
 }
