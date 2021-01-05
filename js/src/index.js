cb493e10533ab02f11f2dcac7c565c27c7e7b1dd
aljt <alvinljt@gmail.com>
2021-01-05 14:07:36 +0800

    moved axios to dependecies

--- js/src/index.js
+++ js/src/index.js
@@ -1,3 +1,15 @@
 import './styles.scss';
+import axios from 'axios';
+
+// game initialisation =============
+// make request for all items for gameplay of the ongoing game
+axios.get('/games')
+  .then((res) => {
+    const gameData = res.data;
+  })
+  .catch((error) => {
+    // handle error
+    console.log('get game error', error);
+  });
 
 console.log('hello');
